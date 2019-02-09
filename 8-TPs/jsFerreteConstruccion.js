90a6e450a8633635eebbb78ed67f54ad239e86b5
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-02-09 04:37:23 -0300

    Ferrete facturacion y construccion sin terminar

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,6 +6,21 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largo ;
+	var ancho;
+	var perimetro ;
+	var resultado;
+
+	largo = document.getElementById('Largo').value ;
+	ancho = document.getElementById('Ancho').value ;
+
+	largo = parseInt (largo) ;
+	ancho = parseInt (ancho) ;
+
+	perimetro = (largo + ancho) * 2 ;
+	resultado = perimetro * 3 ;
+
+	alert ("Cantidad de alambre a comprar : " +resultado) ;
 
 }
 function Circulo () 
--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,67 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+	var precioUno ;
+	var precioDos ;
+	var precioTres ;
+	var suma ;
+
+
+	precioUno = document.getElementById('PrecioUno').value ;
+	precioDos = document.getElementById('PrecioDos').value ;
+	precioTres = document.getElementByid('PrecioTres').value ;
+
+	precioUno = parseInt (precioUno) ;
+	precioDos = parseInt (precioDos) ;
+	precioTres = parseInt (precioTres) ;
+
+	suma = (precioUno + precioDos + precioTres) ;
+
+	alert ("El resultado es" +suma) ;
 	
 }
 function Promedio () 
 {
+	var precioUno ;
+	var precioDos ;
+	var precioTres ;
+	var suma ;
+	var promedio ;
+
+	precioUno = document.getElementById('PrecioUno').value ;
+	precioDos = document.getElementById('PrecioDos').value ;
+	precioTres = document.getElementByid('PrecioTres').value ;
+
+	precioUno = parseInt(precioUno) ;
+	precioDos = parseInt(precioDos) ;
+	precioTres = parseInt(precioTres) ;
+
+	suma = (precioUno + precioDos + precioTres) ;
+	promedio = (suma / 3) ;
+
+	alert ("El promedio es" +promedio) ;
 	
 }
 function PrecioFinal () 
 {
+
+var precioUno ;
+var precioDos ;
+var precioTres ;
+var suma ;
+var resultado ;
+
+precioUno = document.getElementById('PrecioUno').value ;
+precioDos = document.getElementById('PrecioDos').value ;
+precioTres = document.getElementByid('PrecioTres').value ;
+
+precioUno = parseInt (precioUno) ;
+precioDos = parseInt (precioDos) ;
+precioTres = parseInt (precioTres) ;
+
+suma = (precioUno + precioDos + precioTres) ;
+resultado = (suma * 121 / 100) ;
+
+alert ("Precio Final:" +resultado) ;
 	
 }
