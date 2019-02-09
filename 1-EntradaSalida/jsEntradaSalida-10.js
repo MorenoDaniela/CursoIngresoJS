1abc7790d7f2197b35dc5cf80b3164be13f0a627
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-02-09 03:19:43 -0300

    Ejercicio 10 terminado

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,19 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+
+
+var numeroUno ;
+var resultado ;
+
+numeroUno = document.getElementById('importe').value ;
+resultado = document.getElementById('resultado').value ;
+
+numeroUno = parseInt (numeroUno) ;
+resultado = (numeroUno * 25 / 100) ;
+
+alert ("El descuento es: " +resultado) ;
 	
+
+
 }
