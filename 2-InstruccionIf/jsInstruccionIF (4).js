3c067b3991ff39d4d322f0f44df63e7b97abb860
alumno <alumno>
2019-02-11 20:35:01 -0300

    If 5 terminado

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,26 @@ function mostrar()
 {
 //tomo la edad  
 
+var Edad ;
+
+Edad = edad.value ;
+
+Edad = parseInt (Edad) ;
+
+
+// if (Edad > 12 && Edad < 18) es lo mismo a las lineas de abajo//
+
+if (Edad > 12)
+if (Edad < 18)
+{ 
+
+alert ("Es adolescente.") ;
+
+
+
+}
+
+
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,21 @@
 function mostrar()
 {
 //tomo la edad  
+var Edad ;
+
+Edad = edad.value ;
+
+Edad = parseInt (Edad) ;
+
+
+if (Edad < 13 || Edad > 17) 
+{ 
+
+alert ("No es adolescente.") ;
+
+
+
+}
 
 
 }//FIN DE LA FUNCIÓN
