eb08a2d3068ceb0a3768d462efb6fa695e2740b1
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-02-12 00:59:42 -0300

    If 7 terminado

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -11,10 +11,13 @@ Estado = estadoCivil.value ;
 
 Edad = parseInt (Edad) ;
 
-if (Edad < 18 && Soltero)
+if (Edad < 18 && Estado !="Soltero")
+	//el valor de soltero debe estar entre "" y en mayuscula para que me tome el Id
+	
+	{
 
 	alert ("Es muy pequeño para no ser soltero.") ;
 	
-
+	}
 
 }//FIN DE LA FUNCIÓN
