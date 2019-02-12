403ef866795ca69c28ff0e9cb9859ed89a6fa172
alumno <alumno>
2019-02-11 21:26:31 -0300

    If 6 dos maneras

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -7,14 +7,14 @@ Edad = edad.value ;
 
 Edad = parseInt (Edad) ;
 
-
+/*
 if ( Edad > 17) 
 {
 
 alert ("Es mayor de edad.") ;
 }
 
-//if (Edad > 12 && Edad >18)//eror
+
 if (Edad > 12 && Edad < 18) 
 {
 
@@ -25,6 +25,25 @@ if (Edad <13)
 {
 alert ("Es un niño.") ;
 }
+*/
+
+if (Edad > 17) 
+	{
+
+		alert ("Es mayor de edad.") ;
+	}
+		else
+		{
+			if (Edad < 13)
+			{
+				alert("Es un niño");
+			}
+			else
+			{
+				alert("Es un adolescente");
+			}
+		}
+
 
 
 
