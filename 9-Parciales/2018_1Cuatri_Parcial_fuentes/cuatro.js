d9500aa800e16f4c400ee06402cfe8eda303bc65
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-02-13 13:57:02 -0300

    Parcial ejercicio 4 terminado

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -8,34 +8,42 @@ var resultado ;
 numeroUno = prompt ("Por favor ingrese un numero") ;
 numeroDos = prompt ("Por favor ingrese un segundo numero") ;
 
+
+
+
 	if (numeroUno == numeroDos)
 	{
 		resultado = numeroUno + numeroDos ;
 		alert (resultado) ;
 	}
-		else 
-		{	
-			numeroUno = parseInt (numeroUno) ;
-			numeroDos = parseInt (numeroDos) ;
+	else 
+	{	
+		numeroUno = parseInt (numeroUno) ;
+		numeroDos = parseInt (numeroDos) ;
 
 			if (numeroUno > numeroDos)
-			{
-				resultado = numeroUno - numeroDos ;
-				alert (resultado) ;
-			}
-				else (numeroUno < numeroDos)
+
+				{
+					resultado = numeroUno - numeroDos ;
+					alert (resultado) ;
+				}
+			else 
+				//(numeroUno < numeroDos) no se pone porque serian los contrarios es decir en los que el numeroDos es el mayor
 				{
 					resultado = numeroUno + numeroDos ;
+
 					if (resultado > 10)
 
 					{
-						alert ("La suma es" +resultado+ "y supero el 10.") ;
+
+						alert ("La suma es " +resultado+ " y supero el 10.") ;
 
 					}
-						else 
-						{
-							alert (resultado) ;
-						}
+
+					else 
+					{
+						alert (resultado) ;
+					}
 				}
-		}
+	}
 }
