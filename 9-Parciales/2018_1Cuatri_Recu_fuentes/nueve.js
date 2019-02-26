42337e8b8cf457c771f84f6f6f23444f7677b260
MorenoDaniela <denu.moreno.1990@gmail.com>
2019-02-26 11:03:36 -0300

    Iteraciones 7, 8 y 9 recu

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/nueve.js
@@ -1,4 +1,88 @@
 function mostrar()
 {
+	var nombre;
+	var peso;
+	var temperatura;
+	var respuesta = "si";
+	var temperaturasPares=0;
+	var animalMasPesado;
+	var temperaturaAnimalPesado;
+	var animalesAMenosCero = 0;
+	var contadorPeso=0;
+	var promedio;
+	var pesoMaximo = 1001;
+	var pesoMinimo = 0;
 
+
+	
+
+	while (respuesta != "no")
+	{
+		nombre = prompt ("Ingrese el nombre del animal.");
+		while (isNaN(nombre ==true))
+		{
+			nombre = prompt ("ERROR, ingrese el nombre del animal.")
+		}
+
+		peso = prompt ("Ingrese el peso del animal, entre 0 y 1000.");
+		peso = parseInt (peso);
+		while (isNaN(peso) || peso<0 || peso>1000)
+		{
+			peso = prompt ("ERROR, ingrese el peso del animal, entre 0 y 1000.");
+			peso = parseInt(peso);
+		}
+
+		temperatura = prompt ("Ingrese la temperatura del habitat.");
+		temperatura = parseInt (temperatura);
+		while (isNaN(temperatura) || temperatura < -30 || temperatura > 30)
+		{
+			temperatura = prompt ("ERROR, ingrese la temperatura del habitat.");
+			temperatura = parseInt (temperatura);
+		}
+
+
+		if (temperaturasPares%2==0)
+		{
+			temperaturasPares = temperaturasPares + 1 ;
+		}
+
+		if (peso>0)
+		{
+			animalMasPesado = nombre;
+			temperaturaAnimalPesado = temperatura;
+		}
+
+		if (temperatura <0)
+		{
+			animalesAMenosCero = animalesAMenosCero + 1;
+		}
+
+		if (peso>0)
+		{
+			contadorPeso = contadorPeso + 1 ;
+			promedio = contadorPeso + peso ;
+		}
+
+		if (temperatura <0)
+		{
+			if (peso > pesoMinimo)
+			pesoMinimo = peso;
+			else 
+				if (peso < pesoMaximo)
+				{
+					pesoMaximo = peso;
+				}
+		}
+
+	respuesta = prompt ("Desea ingresar otro animal? si o no.");
+
+	}
+
+promedio = promedio / contadorPeso;
+
+alert ("La cantidad de temperaturas pares es " +temperaturasPares);
+alert ("El nombre del animal mas pesado es" +animalMasPesado+ " y su temperatura es " +temperaturaAnimalPesado);
+alert ("La cantidad de animales que viven a menos de 0 grados es " +animalesAMenosCero);
+alert ("El promedio del peso de todos los animales es: "+promedio);
+alert ("El peso maximo de los animales bajo cero es" +pesoMaximo+" y el minimo es " +pesoMinimo);
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -3,31 +3,95 @@ function mostrar()
 	var letra;
 	var numero;
 	var respuesta= "si" ;
-	var contador= 0 ;
+	var contadorPares= 0 ;
+	var contadorCeros=0;
+	var contadorImpares=0 ;
+	var positivo=0;
+	var negativo=0;
 	var promedio;
-	var acumulador= 0;
-
-	numero = prompt ("Ingrese un numero") ;
-	numero = parseInt (numero) ;
-
-	while (respuesta == si)
-	{
-		while (isNaN(numero) ==true || numero < -100 || numero > 100)
-		{
-			numero = prompt ("ERROR, ngrese un numero") ;
-			numero = parseInt (numero) ;
-			letra = prompt ("Ingrese una letra")
-		}
-
-		while (isNaN(letra) ==false)
-		{
-			letra = prompt ("ERROR, ingrese una letra") ;
-			respuesta = prompt ("Desea ingresar otro nùmero? si o no")
-		}
-
-		
-	}
-
+	var maximo= -101;
+	var letraMaximo;
+	var minimo= 101;
+	var letraMinimo;
 
 	
+
+
+
+	while (respuesta != "no")
+	{
+		numero = prompt ("Ingrese un numero entre -100 y 100.") ;
+		numero = parseInt (numero) ;
+
+		while (isNaN(numero) || numero < -100 || numero > 100)
+		{
+			numero = prompt ("ERROR, ingrese un numero entre -100 y 100.") ;
+			numero = parseInt (numero) ;
+		}
+
+		letra = prompt ("Ingrese una letra");
+
+		while (isNaN(letra ==true))
+		{
+			letra = prompt ("ERROR, ingrese una letra válida") ;
+		}
+
+		if (numero==0)
+		{
+			//contadorCeros + 1 ;
+			contadorCeros++;
+		}
+
+		if (numero % 2 == 0)
+		{
+			//contadorPares = contadorPares + 1 ;
+			contadorPares++;
+		}
+			else 
+			{
+				//contadorImpares = contadorImpares + 1 ;
+				contadorImpares++;
+			}
+		
+		
+			
+	
+
+		if (numero>0)
+		{
+			positivo= positivo + 1;
+			promedio = promedio + numero;
+		}
+			else
+			{
+				if (numero<0)
+				{
+					negativo = negativo + numero;
+				}
+			}
+		
+
+		if (numero < minimo)
+		{
+			minimo = numero;
+			letraMinimo = letra;
+		}
+
+		if (numero > maximo)
+		{
+			maximo = numero;
+			letraMaximo = letra;
+		}
+
+		respuesta = prompt ("Desea ingresar otro nùmero? si o no");
+	}
+
+	promedio = promedio / positivo;
+	
+	alert ("La cantidad de numeros pares es" +contadorPares);
+	alert ("La cantidad de numeros impares es" +contadorImpares);
+	alert ("La cantidad de ceros es" +contadorCeros);
+	alert ("El promedio de todos los numeros positivos es" +promedio);
+	alert ("La suma de todos los numeros negativos es" +negativo);
+	alert ("El numero maximo es " +maximo+ " y su letra "+letraMaximo+ " el numero minimo es "+minimo*" y su letra " +letraMinimo);
 }
